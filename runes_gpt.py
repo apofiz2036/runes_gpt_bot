603d1e8108ff39499fefa48ee2240bdba63c03c8
Apofiz <mikhailov_ya@mail.ru>
2025-07-30 23:09:49 +0500

    добавил гадание на трёх рунах

--- runes_gpt.py
+++ runes_gpt.py
@@ -23,6 +23,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
 
 SUBSCRIBERS_FILE = "subscribers.csv"
+SINGLE_RUNES = ["dagaz", "eihwaz", "gebo", "hagalaz", "inguz", "isa", "jera", "sowilo"]
 
 with open('bot_description.txt', 'r', encoding='utf-8') as file:
     BOT_DESCRIPTION = file.read()
@@ -64,14 +65,12 @@ def load_rune_data():
         return data['one_rune']
     
 
-def get_random_rune():
+def get_random_one_rune():
     rune_data = load_rune_data()
     all_runes = list(rune_data.keys())
     random_rune = random.choice(all_runes)
 
-    single_runes = ["dagaz", "eihwaz", "gebo", "hagalaz", "inguz", "isa", "jera", "sowilo"]
-
-    if random_rune in single_runes:
+    if random_rune in SINGLE_RUNES:
         name = rune_data[random_rune]['name']
         image = rune_data[random_rune]['image']
     else:
@@ -83,13 +82,45 @@ def get_random_rune():
     return name, image_path
 
 
-def load_prompt():
-    with open('prompt.txt', 'r', encoding='utf-8') as f:
+def get_random_three_runes():
+    rune_data = load_rune_data()
+    all_runes_keys = list(rune_data.keys())
+    selected_runes = random.sample(all_runes_keys, 3)
+
+    result = []
+    for rune_key in selected_runes:
+        if rune_key in SINGLE_RUNES:
+            variant = None
+        else:
+            variant = random.choice(list(rune_data[rune_key].keys()))
+
+        result.append({
+            'rune_key': rune_key,
+            'variant': variant
+        })
+    
+    return result
+
+
+def load_prompt(prompt_type: str = 'one_rune'):
+    file_name = f'prompt_{prompt_type}.txt'
+    with open(file_name, 'r', encoding='utf-8') as f:
         return f.read()
 
 
-async def ask_gpt(user_question: str, rune_name: str):
-    prompt = load_prompt().format(question=user_question, rune=rune_name)
+async def ask_gpt(user_question: str, rune_data: dict, prompt_type: str = 'one_rune'):
+    if prompt_type == 'one_rune':
+        prompt = load_prompt('one_rune').format(
+            question=user_question,
+            rune=rune_data['name']
+        )
+    elif prompt_type == 'three_runes':
+        prompt = load_prompt('three_runes').format(
+            question=user_question,
+            rune1=rune_data[0]['name'],
+            rune2=rune_data[1]['name'],
+            rune3=rune_data[2]['name'],
+        )
 
     url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
     headers = {
@@ -121,7 +152,24 @@ async def one_rune_mode(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
     context.user_data['mode'] = 'one_rune'
+    context.user_data['prompt_type'] = 'one_rune'
     await update.message.reply_text("Задайте ваш вопрос для гадания на одной руне:", reply_markup=reply_markup)
+
+
+async def three_runes_mode(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    keyboard = [
+        ['Главное меню']
+    ]
+    reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
+    context.user_data['mode'] = 'three_rune'
+    context.user_data['prompt_type'] = 'three_runes'
+
+    context.user_data['selected_runes'] = get_random_three_runes()
+    
+    await update.message.reply_text(
+        "Задайте ваш вопрос для гадания на трёх рунах",
+        reply_markup=reply_markup
+    )
 
 
 async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -130,7 +178,7 @@ async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if text == "Одна руна":
         await one_rune_mode(update, context)
     if text == "Три руны":
-        await update.message.reply_text("Выбрано три руны (функционал в разработке)")
+        await three_runes_mode(update, context)
     if text == "Как гадать":
         await update.message.reply_text("Выбрано как гадать (функционал в разработке)")
     if text == "Главное меню":
@@ -138,7 +186,9 @@ async def handle_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if context.user_data.get('mode') != 'one_rune':
+    current_mode = context.user_data.get('mode')
+
+    if current_mode not in ['one_rune', 'three_rune']:
         await main_menu(update, context)
         return
 
@@ -146,16 +196,39 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         user = update.message.from_user
         save_subscriber(user.id)
         context.user_data['is_subscribed'] = True
-
+    
     user_question = update.message.text
-    rune_name, rune_image = get_random_rune()
 
-    with open(rune_image, 'rb') as photo:
-        await update.message.reply_photo(photo)
+    if current_mode == 'one_rune':
+        rune_name, rune_image = get_random_one_rune()
+        with open(rune_image, 'rb') as photo:
+            await update.message.reply_photo(photo)
+        gpt_response = await ask_gpt(user_question, {'name': rune_name}, 'one_rune')
+    elif current_mode == 'three_rune':
+        runes = context.user_data['selected_runes']
+        rune_data = load_rune_data()
 
-    gpt_response = await ask_gpt(user_question, rune_name)
+        for rune in runes:
+            if rune['variant'] is None:
+                image = rune_data[rune['rune_key']]['image']
+            else:
+                image = rune_data[rune['rune_key']][rune['variant']]['image']
+            
+            image_path = os.path.join('images', image)
+            with open(image_path, 'rb') as photo:
+                await update.message.reply_photo(photo)
+
+        runes_for_prompt = []
+        for rune in runes:
+            if rune['variant'] is None:
+                name = rune_data[rune['rune_key']]['name']
+            else:
+                name = rune_data[rune['rune_key']][rune['variant']]['name']
+            runes_for_prompt.append({'name': name})
+
+        gpt_response = await ask_gpt(user_question, runes_for_prompt, 'three_runes')
+
     await update.message.reply_text(gpt_response)
-
     await main_menu(update, context)
 
 
